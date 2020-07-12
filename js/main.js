f4deace34b74c259c32b59ca7dac263da0a12dac
muhammadjewel <msuratov@gmail.com>
2020-07-12 12:17:10 +0500

    Create Telegram subscription modal

--- js/main.js
+++ js/main.js
@@ -17,5 +17,38 @@ ready(function() {
   elSiteHeaderToggler.addEventListener('click', function () {
     elSitenav.classList.toggle('sitenav--open');
   });
+
+
+  // Open/close modal
+  var elModal = document.querySelector('.modal');
+  var elModalClose = elModal.querySelector('.modal__close');
+
+  var closeModal = function () {
+    elModal.classList.remove('modal--open');
+  };
+  
+  var openModal = function () {
+    elModal.classList.add('modal--open');
+  };
+
+  if (elModalClose) {
+    elModalClose.addEventListener('click', function () {
+      closeModal();
+    });
+  }
+
+  if (elModal) {
+    elModal.addEventListener('click', function (evt) {
+      if (evt.target.matches('.modal')) {
+        closeModal();
+      }
+    });
+
+    document.body.addEventListener('mouseleave', function () {
+      openModal();
+    }, {
+      once: true
+    });
+  }
   
 });
